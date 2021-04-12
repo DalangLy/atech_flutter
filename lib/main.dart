73cbeff1bf87267c88652aa1b927fe598472bbe2
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2021-04-13 06:37:38 +0700

    add map object

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,18 +14,31 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      _questionIndex = _questionIndex+1;
+      _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var gg = [
+      {5: 44, 4: 12}
+    ];
     var questions = [
-      'what is your favourite color?',
-      'what is your favourite animal?'
+      {
+        'questionText': 'what is your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'what is your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      },
+      {
+        'questionText': 'who is your favorite instructor?',
+        'answers': ['Max', 'Max', 'Max', 'Max']
+      }
     ];
     return MaterialApp(
       home: new Scaffold(
@@ -33,15 +47,11 @@ class _MyAppState extends State<MyApp> {
         ),
         body: new Column(
           children: [
-            new Question(questions[_questionIndex]),
-            RaisedButton(
-                child: new Text('Answer 1'), onPressed: answerQuestion),
-            RaisedButton(
-                child: new Text('Answer 2'), onPressed: answerQuestion),
-            RaisedButton(
-              child: new Text('Answer 3'),
-              onPressed: () => print('hello world'),
-            )
+            new Question(questions[_questionIndex]['questionText']),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
